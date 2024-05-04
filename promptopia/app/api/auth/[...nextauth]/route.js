fbf3c024d47fea984e15901cb0627f24f32deab4
SheriffKoder <sheriff.koder@hotmail.com>
2024-05-04 12:01:31 +0300

    deployment re-adjustment

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -23,6 +23,7 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    secret: process.env.NEXT_PUBLIC_SECRET,
     callbacks: {
         async session({ session }) {
             const sessionUser = await User.findOne({email: session.user.email});
